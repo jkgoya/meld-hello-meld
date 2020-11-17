3dc16b2fea80cf6c9ded52e0c98f01fc5aaa3334
Graham Klyne <gk-github@ninebynine.org>
2020-11-17 13:00:14 +0000

    Remove unused imports

--- hello-meld-2/src/index.js
+++ hello-meld-2/src/index.js
@@ -1,10 +1,9 @@
-import React, { Component }  from 'react';
+import React  from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import ReduxPromise from 'redux-promise';
-import { Router, Route, browserHistory } from 'react-router'
 
 import { reducers } from 'meld-clients-core/lib/reducers';
 import Score from 'meld-clients-core/lib/containers/score';
