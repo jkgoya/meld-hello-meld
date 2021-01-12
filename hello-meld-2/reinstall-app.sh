7a670a9a202f2cd12693c2ce12e0e3a29d88b401
Graham Klyne <gk-github@ninebynine.org>
2021-01-12 14:02:51 +0000

    Remove react-media-player dependency in hello-meld-2. Bump react and react-dom version deopendencies to 17.0.0.
    NOTE: there are dev dependencies that generate multiple deprecated package warnings.
    The warnings occur on a first re-build after package-lock.json and node_modules are
    removed; after that the warnings do not appear.  Apparently.

--- hello-meld-2/reinstall-app.sh
+++ hello-meld-2/reinstall-app.sh
@@ -1,7 +1,10 @@
 # Reinstall current MELD app using local copy of meld-clients-core
 #
 
-rm package-lock.js
+# For some reason, removing package-lock.json causes all sorts of warnings about obsolete packages,
+# but leaving it, the next attempt to re-build completes cleanly, despite node_modules being reset.
+#   rm package-lock.json
+
 rm -rf node_modules
 npm link meld-clients-core
 npm install
