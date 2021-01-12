79a9b61b143b28b69e48192d7d58cb4b8c047ff8
Graham Klyne <gk-github@ninebynine.org>
2021-01-12 10:52:23 +0000

    Continuing work in progress to refactor hello-meld-2 for testing. Updated hello-meld-2/package.json and reinstall-app.sh now build cleanly, and work with updated meld-clients-core (see previous commit).

--- hello-meld-2/reinstall-app.sh
+++ hello-meld-2/reinstall-app.sh
@@ -1,11 +1,10 @@
 # Reinstall current MELD app using local copy of meld-clients-core
 #
 
-# rm package-lock.js
-# rm -rf node_modules
+rm package-lock.js
+rm -rf node_modules
+npm link meld-clients-core
 npm install
 npm link meld-clients-core
-# npm link ../../meld-clients-core/node_modules/react
 
 # npm start
-    
