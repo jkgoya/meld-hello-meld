f866509233ed2f8c18cdc672021a8d9231ae7857
Graham Klyne <gk-github@ninebynine.org>
2021-02-02 14:45:27 +0000

    hello-meld-3 build and start OK (with clean build of meld-clients-core)

--- hello-meld-3/reinstall-app.sh
+++ hello-meld-3/reinstall-app.sh
@@ -1,10 +1,14 @@
 # Reinstall current MELD app using local copy of meld-clients-core
 #
 
-# rm package-lock.js
-# rm -rf node_modules
+if [[ "$1" == "clean" ]]; then
+    echo "Removing old package-lock.json and node_modules..."
+    rm package-lock.json
+    rm -rf node_modules 
+fi
+
+echo "npm installing..."
 npm link meld-clients-core
 npm install
 npm link meld-clients-core
 # npm start
-    
