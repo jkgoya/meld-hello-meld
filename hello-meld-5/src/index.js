f8ef2ea7fad06f81d53f069ee237448f6ceb8346
Graham Klyne <gk-github@ninebynine.org>
2021-04-27 13:37:13 +0100

    Fix index.js and style.css for hello-meld-5; add sample annotation for link to recording

--- hello-meld-5/src/index.js
+++ hello-meld-5/src/index.js
@@ -3,24 +3,28 @@ import ReactDOM from 'react-dom';
 
 import SelectableScoreWrapper from './containers/selectableScoreWrapper';
 
-ReactDOM.render(
-  <SelectableScoreWrapper
-  // vrvOptions={vrvOptions}
-  // zoomIn={onZoomIncrease}
-  // zoomOut={onZoomDecrease}
-  />,
-  document.querySelector(".container")
-);
-
 // ReactDOM.render(
-//   <div>
-//     <h1>Hello MELD (5)</h1>
-//     <div className="wrapscorepane">
-//       <React.StrictMode>
-//         <SelectableScoreWrapper uri = { MEI_URI } vrvOptions = { vrvOptions }/>
-//       </React.StrictMode>
-//     </div>
-//   </div>
-//   , document.querySelector('.container')
+//   <SelectableScoreWrapper
+//   // vrvOptions={vrvOptions}
+//   // zoomIn={onZoomIncrease}
+//   // zoomOut={onZoomDecrease}
+//   />,
+//   document.querySelector(".container")
 // );
+
+ReactDOM.render(
+  <div>
+    <h1>Hello MELD (5)</h1>
+    <div className="wrapscorepane">
+      <React.StrictMode>
+        <SelectableScoreWrapper
+        // vrvOptions={vrvOptions}
+        // zoomIn={onZoomIncrease}
+        // zoomOut={onZoomDecrease}
+        />,
+      </React.StrictMode>
+    </div>
+  </div>
+  , document.querySelector('.container')
+);
 
